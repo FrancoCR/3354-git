69ab78b5593d84213595645e6d39f656315a08f0
Franco Covarrubias-Reyes <fcovarrubiasreyes@yahoo.com>
2017-01-29 13:43:48 -0600

    Updated Adder
    Change the function addArguments so that it can add numerous numbers rather then just 2, also updated the statement the catch prints

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -2,16 +2,31 @@ package adder;
 
 public class Main {
 
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args) 
+    {
+        try 
+        {
             int result = addArguments(args);
             System.out.println(result);
-        } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+        }
+        catch (Exception e)
+        {
+            System.err.println("Please provide a list of integers to add");
         }
     }
 
-    private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+    private static int addArguments(String[] args)
+    {
+        int i = 0;
+	    int j = 1;
+	    int addFinal =0;
+	    int argumentLength = args.length;
+	    while (j < argumentLength)
+	    {
+	      addFinal = Integer.valueOf(args[i]) + Integer.valueOf(args[j]);
+	      i++;
+	      j++;
+	    }
+	    return addFinal;
     }
 }
